c7ce18b1dccbee3a3f86f17b30f2fa9ab9151bcb
znxftw <vishnu2101@gmail.com>
2021-09-08 03:20:43 +0530

    Extract method for finding bit

--- Chess/Bitboard.cs
+++ Chess/Bitboard.cs
@@ -18,11 +18,15 @@ namespace Chess
                 for (int file = 0; file < 8; ++file)
                 {
                     int square = (rank * 8) + file;
-                    ulong bit = Board & ((ulong)1 << square);
-                    Console.Write(bit > 0 ? 1 : 0);
+                    Console.Write(BitAt(square));
                 }
                 Console.Write(Environment.NewLine);
             }
+        }
+
+        private int BitAt(int square)
+        {
+            return (Board & ((ulong)1 << square)) > 0 ? 1 : 0;
         }
     }
 }
