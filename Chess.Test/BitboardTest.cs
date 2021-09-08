9f067f07060511ecf0ed06c6aac4457527777bd3
znxftw <vishnu2101@gmail.com>
2021-09-08 23:17:59 +0530

    Add basic test for SetBit

--- Chess.Test/BitboardTest.cs
+++ Chess.Test/BitboardTest.cs
@@ -4,5 +4,17 @@ namespace Chess.Test
 {
     public class BitboardTest
     {
+        [Fact]
+        public void ShouldSetSpecifiedBits()
+        {
+            var Board = new Bitboard(0);
+            Assert.Equal((ulong)0, Board.Board);
+
+            Board.SetBit(5);
+            Assert.Equal((ulong)32, Board.Board);
+
+            Board.SetBit(63);
+            Assert.Equal(9223372036854775840, Board.Board);
+        }
     }
 }
