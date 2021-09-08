56efe9c995957765cc271473c62fd2cbbbfa0939
znxftw <vishnu2101@gmail.com>
2021-09-09 04:02:38 +0530

    Split lookups into partial class

---
+++ Chess/Bitboard.Lookups.cs
@@ -0,0 +1,21 @@
+﻿namespace Chess
+{
+    public partial class Bitboard
+    {
+        private static readonly ulong FileA = 72340172838076673;
+        private static readonly ulong FileB = 144680345676153346;
+        private static readonly ulong FileG = 4629771061636907072;
+        private static readonly ulong FileH = 9259542123273814144;
+        private static readonly ulong FileAB = FileA | FileB;
+        private static readonly ulong FileGH = FileG | FileH;
+
+        public static readonly ulong[,] PawnAttacks = new ulong[Constants.Sides, Constants.Squares];
+
+        static Bitboard()
+        {
+            for (int side = 0; side < Constants.Sides; ++side)
+                for (int square = 0; square < Constants.Squares; ++square)
+                    PawnAttacks[side, square] = Bitboard.GetPawnAttacks((Square)square, (Side)side).Board;
+        }
+    }
+}
--- Chess/Bitboard.cs
+++ Chess/Bitboard.cs
@@ -6,27 +6,10 @@ namespace Chess
     {
         public ulong Board { get; set; }
 
-        // Precalculated Bitboards
-        private static readonly ulong FileA = 72340172838076673;
-        private static readonly ulong FileB = 144680345676153346;
-        private static readonly ulong FileG = 4629771061636907072;
-        private static readonly ulong FileH = 9259542123273814144;
-        private static readonly ulong FileAB = FileA | FileB;
-        private static readonly ulong FileGH = FileG | FileH;
-
-        // Precalculated Attacks
-        public static readonly ulong[,] PawnAttacks = new ulong[Constants.Sides, Constants.Squares];
 
         public Bitboard(ulong board)
         {
             Board = board;
-        }
-
-        static Bitboard()
-        {
-            for (int side = 0; side < Constants.Sides; ++side)
-                for (int square = 0; square < Constants.Squares; ++square)
-                    PawnAttacks[side, square] = Bitboard.GetPawnAttacks((Square)square, (Side)side).Board;
         }
 
         public void Print()
