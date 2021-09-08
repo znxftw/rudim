80c6f5da33e805cccc8d6544cfb1f31cd3fee272
znxftw <vishnu2101@gmail.com>
2021-09-08 23:53:52 +0530

    Debug

--- Chess/Program.cs
+++ Chess/Program.cs
@@ -4,9 +4,9 @@
     {
         static void Main(string[] args)
         {
-            var Board = new Bitboard(128);
-            Board.SetBit(15);
-            Board.ClearBit(15);
+            var Board = new Bitboard(1);
+            Board.SetBit(Square.e8);
+            Board.ClearBit(Square.a8);
 
             Board.Print();
         }
