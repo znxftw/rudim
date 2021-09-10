a39743296edbbc753ce7e01a3d64ff01b3e0d0b6
znxftw <vishnu2101@gmail.com>
2021-09-10 23:30:52 +0530

    Add mask generation functions

---
+++ Rudim.Test/BitboardMagicsTest.cs
@@ -0,0 +1,86 @@
+﻿using System.Numerics;
+using Xunit;
+
+namespace Rudim.Test
+{
+    public class BitboardMagicsTest
+    {
+        [Fact]
+        public void ShouldGetMaskForCentralBishop()
+        {
+            var BishopMaskE5 = Bitboard.GetBishopMask(Square.e5);
+
+            Assert.Equal(1, BishopMaskE5.GetBit(Square.f4));
+            Assert.Equal(1, BishopMaskE5.GetBit(Square.g3));
+
+            Assert.Equal(1, BishopMaskE5.GetBit(Square.f6));
+            Assert.Equal(1, BishopMaskE5.GetBit(Square.g7));
+
+            Assert.Equal(1, BishopMaskE5.GetBit(Square.d4));
+            Assert.Equal(1, BishopMaskE5.GetBit(Square.c3));
+            Assert.Equal(1, BishopMaskE5.GetBit(Square.b2));
+
+            Assert.Equal(1, BishopMaskE5.GetBit(Square.d6));
+            Assert.Equal(1, BishopMaskE5.GetBit(Square.c7));
+
+            Assert.Equal(9, BitOperations.PopCount(BishopMaskE5.Board));
+        }
+
+
+        [Fact]
+        public void ShouldGetMaskForCornerBishop()
+        {
+            var BishopMaskA1 = Bitboard.GetBishopMask(Square.a1);
+
+            Assert.Equal(1, BishopMaskA1.GetBit(Square.b2));
+            Assert.Equal(1, BishopMaskA1.GetBit(Square.c3));
+            Assert.Equal(1, BishopMaskA1.GetBit(Square.d4));
+            Assert.Equal(1, BishopMaskA1.GetBit(Square.e5));
+            Assert.Equal(1, BishopMaskA1.GetBit(Square.f6));
+            Assert.Equal(1, BishopMaskA1.GetBit(Square.g7));
+            Assert.Equal(6, BitOperations.PopCount(BishopMaskA1.Board));
+        }
+
+        [Fact]
+        public void ShouldGetMaskForCentralRook()
+        {
+            var RookMaskE5 = Bitboard.GetRookMask(Square.e5);
+
+            Assert.Equal(1, RookMaskE5.GetBit(Square.e2));
+            Assert.Equal(1, RookMaskE5.GetBit(Square.e3));
+            Assert.Equal(1, RookMaskE5.GetBit(Square.e4));
+            Assert.Equal(1, RookMaskE5.GetBit(Square.e6));
+            Assert.Equal(1, RookMaskE5.GetBit(Square.e7));
+
+            Assert.Equal(1, RookMaskE5.GetBit(Square.b5));
+            Assert.Equal(1, RookMaskE5.GetBit(Square.c5));
+            Assert.Equal(1, RookMaskE5.GetBit(Square.d5));
+            Assert.Equal(1, RookMaskE5.GetBit(Square.f5));
+            Assert.Equal(1, RookMaskE5.GetBit(Square.g5));
+
+            Assert.Equal(10, BitOperations.PopCount(RookMaskE5.Board));
+        }
+
+        [Fact]
+        public void ShouldGetMaskForCornerRook()
+        {
+            var RookMaskA1 = Bitboard.GetRookMask(Square.a1);
+
+            Assert.Equal(1, RookMaskA1.GetBit(Square.a2));
+            Assert.Equal(1, RookMaskA1.GetBit(Square.a3));
+            Assert.Equal(1, RookMaskA1.GetBit(Square.a4));
+            Assert.Equal(1, RookMaskA1.GetBit(Square.a5));
+            Assert.Equal(1, RookMaskA1.GetBit(Square.a6));
+            Assert.Equal(1, RookMaskA1.GetBit(Square.a7));
+
+            Assert.Equal(1, RookMaskA1.GetBit(Square.b1));
+            Assert.Equal(1, RookMaskA1.GetBit(Square.c1));
+            Assert.Equal(1, RookMaskA1.GetBit(Square.d1));
+            Assert.Equal(1, RookMaskA1.GetBit(Square.e1));
+            Assert.Equal(1, RookMaskA1.GetBit(Square.f1));
+            Assert.Equal(1, RookMaskA1.GetBit(Square.g1));
+
+            Assert.Equal(12, BitOperations.PopCount(RookMaskA1.Board));
+        }
+    }
+}
---
+++ Rudim/Bitboard/Bitboard.Magics.cs
@@ -0,0 +1,51 @@
+﻿namespace Rudim
+{
+    public partial class Bitboard
+    {
+        public static Bitboard GetBishopMask(Square square)
+        {
+            var ResultBoard = new Bitboard(0);
+            // Masking equivalent to zero blockers and no edge square
+            var blockers = new Bitboard(0);
+            var BishopRank = (int)square / 8;
+            var BishopFile = (int)square % 8;
+
+            for (int rank = BishopRank + 1, file = BishopFile + 1; rank < 7 && file < 7; ++rank, ++file)
+                if (AddSquareToBoardAndStopAtBlockers(ResultBoard, rank, file, blockers)) break;
+
+            for (int rank = BishopRank - 1, file = BishopFile + 1; rank >= 1 && file < 7; --rank, ++file)
+                if (AddSquareToBoardAndStopAtBlockers(ResultBoard, rank, file, blockers)) break;
+
+            for (int rank = BishopRank - 1, file = BishopFile - 1; rank >= 1 && file >= 1; --rank, --file)
+                if (AddSquareToBoardAndStopAtBlockers(ResultBoard, rank, file, blockers)) break;
+
+            for (int rank = BishopRank + 1, file = BishopFile - 1; rank < 7 && file >= 1; ++rank, --file)
+                if (AddSquareToBoardAndStopAtBlockers(ResultBoard, rank, file, blockers)) break;
+
+            return ResultBoard;
+        }
+
+        public static Bitboard GetRookMask(Square square)
+        {
+            var ResultBoard = new Bitboard(0);
+            // Masking equivalent to zero blockers and no edge square
+            var blockers = new Bitboard(0);
+            var RookRank = (int)square / 8;
+            var RookFile = (int)square % 8;
+
+            for (int rank = RookRank + 1; rank < 7; ++rank)
+                if (AddSquareToBoardAndStopAtBlockers(ResultBoard, rank, RookFile, blockers)) break;
+
+            for (int rank = RookRank - 1; rank >= 1; --rank)
+                if (AddSquareToBoardAndStopAtBlockers(ResultBoard, rank, RookFile, blockers)) break;
+
+            for (int file = RookFile + 1; file < 7; ++file)
+                if (AddSquareToBoardAndStopAtBlockers(ResultBoard, RookRank, file, blockers)) break;
+
+            for (int file = RookFile - 1; file >= 1; --file)
+                if (AddSquareToBoardAndStopAtBlockers(ResultBoard, RookRank, file, blockers)) break;
+
+            return ResultBoard;
+        }
+    }
+}
