46c29fa98b662c21357dd036cb5818daec2a6e75
znxftw <vishnu2101@gmail.com>
2021-09-13 23:24:03 +0530

    Add BoardState class

---
+++ Rudim/Board/BoardState.cs
@@ -0,0 +1,27 @@
+﻿namespace Rudim.Board
+{
+    public class BoardState
+    {
+        public BoardState()
+        {
+            Pawns = new Bitboard[Constants.Sides];
+            Knights = new Bitboard[Constants.Sides];
+            Bishops = new Bitboard[Constants.Sides];
+            Rooks = new Bitboard[Constants.Sides];
+            Kings = new Bitboard[Constants.Sides];
+            Queens = new Bitboard[Constants.Sides];
+        }
+
+        public Bitboard[] Pawns { get; set; }
+        public Bitboard[] Knights { get; set; }
+        public Bitboard[] Bishops { get; set; }
+        public Bitboard[] Rooks { get; set; }
+        public Bitboard[] Kings { get; set; }
+        public Bitboard[] Queens { get; set; }
+        public Bitboard WhitePieces { get; set; }
+        public Bitboard BlackPieces { get; set; }
+        public Bitboard AllPieces { get; set; }
+        public Side SideToMove { get; set; }
+        public Square EnPassantSquare { get; set; }
+    }
+}
