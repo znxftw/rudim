d4553d0a7372182a3e06d3f99dc938e380e5e85c
znxftw <vishnu2101@gmail.com>
2021-09-15 00:15:46 +0530

    Add skeleton for parsing FEN

---
+++ Rudim.Test/BoardStateFENTest.cs
@@ -0,0 +1,17 @@
+﻿using Rudim.Board;
+using System;
+using Xunit;
+
+namespace Rudim.Test
+{
+    public class BoardStateFENTest
+    {
+        [Fact]
+        public void ShouldParseStartingFENCorrectly()
+        {
+            var fen = "rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1 ";
+
+            Assert.Throws<NotImplementedException>(() => BoardState.ParseFEN(fen));
+        }
+    }
+}
--- Rudim/Board/BoardState.FEN.cs
+++ Rudim/Board/BoardState.FEN.cs
@@ -1,6 +1,39 @@
-﻿namespace Rudim.Board
+﻿using System;
+
+namespace Rudim.Board
 {
     public partial class BoardState
     {
+        public static BoardState ParseFEN(string FEN)
+        {
+            var board = new BoardState();
+            var sections = FEN.Split(' ');
+            ParsePieces(board, sections[0]);
+            ParseSideToMove(board, sections[1]);
+            ParseCastling(board, sections[2]);
+            ParseEnPassant(board, sections[3]);
+            // ParsePly(board, sections[4]);
+            return board;
+        }
+
+        private static void ParseEnPassant(BoardState board, string v)
+        {
+            throw new NotImplementedException();
+        }
+
+        private static void ParseCastling(BoardState board, string v)
+        {
+            throw new NotImplementedException();
+        }
+
+        private static void ParseSideToMove(BoardState board, string v)
+        {
+            throw new NotImplementedException();
+        }
+
+        private static void ParsePieces(BoardState board, string v)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
