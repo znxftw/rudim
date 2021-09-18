40140121befda74996e258ec3d92d0768a9f45eb
znxftw <vishnu2101@gmail.com>
2021-09-19 00:45:53 +0530

    Add initial structure for Move class

---
+++ Rudim/Common/Move.cs
@@ -0,0 +1,9 @@
+﻿namespace Rudim.Common
+{
+    class Move
+    {
+        public Square Source { get; set; }
+        public Square Target { get; set; }
+        public MoveType Type { get; set; }
+    }
+}
---
+++ Rudim/Common/MoveType.cs
@@ -0,0 +1,8 @@
+﻿namespace Rudim.Common
+{
+    public enum MoveType
+    {
+        Quiet,
+        Capture
+    }
+}
