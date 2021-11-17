00cacc280c0a2e641a916686d878fc52f7d8f7ca
znxftw <vishnu2101@gmail.com>
2021-11-18 02:05:47 +0530

    Add starting position perft tests

--- Rudim.Test/Perft/Test.cs
+++ Rudim.Test/Perft/Test.cs
@@ -1,6 +1,59 @@
-﻿namespace Rudim.Test.Perft
+﻿using Rudim.Board;
+using Rudim.Common;
+using Rudim.Test.Common;
+using System;
+using System.Diagnostics;
+using Xunit;
+using Xunit.Abstractions;
+
+namespace Rudim.Test.Perft
 {
-    class Test
+    public class Test
     {
+        private readonly ITestOutputHelper output;
+
+        public Test(ITestOutputHelper output)
+        {
+            this.output = output;
+        }
+
+        [Theory]
+        [InlineData(0, 1)]
+        [InlineData(1, 20)]
+        [InlineData(2, 400)]
+        [InlineData(3, 8_902)]
+        [InlineData(4, 197_281)]
+        [InlineData(5, 4_865_609)]
+        public void PerftStartingPosition(int depth, ulong nodes)
+        {
+            var boardState = BoardState.ParseFEN(Helpers.StartingFEN);
+            PerftDriver.ResetNodeCount();
+            PerftDriver.Traverse(boardState, depth);
+            Assert.Equal(nodes, PerftDriver.nodes);
+        }
+
+        [Fact (Skip= "Unskip when debugging")]
+        public void PerftDebug()
+        {
+            var depth = 3;
+
+            var boardState = BoardState.ParseFEN(Helpers.StartingFEN);
+            ulong total = 0;
+            boardState.GenerateMoves();
+            foreach(var move in boardState.Moves)
+            {
+                PerftDriver.ResetNodeCount();
+                boardState.SaveState();
+                boardState.MakeMove(move);
+
+                if(!boardState.IsInCheck(boardState.SideToMove.Other()))
+                   PerftDriver.Traverse(boardState, depth - 1);
+
+                total += PerftDriver.nodes;
+                output.WriteLine(move.Source.ToString() + move.Target.ToString() + " " + PerftDriver.nodes);
+                boardState.RestoreState();
+            }
+            output.WriteLine(total.ToString());
+        }
     }
 }
