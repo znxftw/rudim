6c3ad389f9be40a0abfde586332dce35b73c5a2a
prashantchoudhary <get.prashant.007@gmail.com>
2022-02-21 21:13:08 +0530

    Code cleanup

--- Rudim/Common/MoveTypes.cs
+++ Rudim/Common/MoveTypes.cs
@@ -1,22 +1,5 @@
 ﻿namespace Rudim.Common
 {
-    // public enum MoveType
-    // {
-    //     Quiet,
-    //     Capture,
-    //     EnPassant,
-    //     DoublePush,
-    //     KnightPromotion = 4,
-    //     BishopPromotion = 5,
-    //     RookPromotion = 6,
-    //     QueenPromotion = 7,
-    //     KnightPromotionCapture = 12,
-    //     BishopPromotionCapture = 13,
-    //     RookPromotionCapture = 14,
-    //     QueenPromotionCapture = 15,
-    //     Castle = 16
-    // }
-
     public record MoveType(int Value, Piece Piece = Piece.None, string PromotionChar = "", bool IsCapture = false);
 
     public static class MoveTypes
