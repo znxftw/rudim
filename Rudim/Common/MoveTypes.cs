ce6c623e1e7ac05e1e2c2ff6cade6d7014c3f78e
znxftw <vishnu2101@gmail.com>
2024-12-05 10:00:19 +0530

    fix: illegal knight promotion LAN [skip ci]

--- Rudim/Common/MoveTypes.cs
+++ Rudim/Common/MoveTypes.cs
@@ -13,11 +13,11 @@ namespace Rudim.Common
         public static MoveType Capture { get; } = new(1, Piece.None, null, true);
         public static MoveType EnPassant { get; } = new(2, Piece.None, null, true);
         public static MoveType DoublePush { get; } = new(3, Piece.None);
-        public static MoveType KnightPromotion { get; } = new(4, Piece.Knight, "k");
+        public static MoveType KnightPromotion { get; } = new(4, Piece.Knight, "n");
         public static MoveType BishopPromotion { get; } = new(5, Piece.Bishop, "b");
         public static MoveType RookPromotion { get; } = new(6, Piece.Rook, "r");
         public static MoveType QueenPromotion { get; } = new(7, Piece.Queen, "q");
-        public static MoveType KnightPromotionCapture { get; } = new(12, Piece.Knight, "k", true);
+        public static MoveType KnightPromotionCapture { get; } = new(12, Piece.Knight, "n", true);
         public static MoveType BishopPromotionCapture { get; } = new(13, Piece.Bishop, "b", true);
         public static MoveType RookPromotionCapture { get; } = new(14, Piece.Rook, "r", true);
         public static MoveType QueenPromotionCapture { get; } = new(15, Piece.Queen, "q", true);
