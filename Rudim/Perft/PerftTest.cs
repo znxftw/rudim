e23f31f9b78d37e6cc653ce8b4c6db3668d7555b
Sanjanarajbk <57423557+Sanjanarajbk@users.noreply.github.com>
2024-12-15 23:07:18 +0530

    Move perft to program (#41)

---
+++ Rudim/Perft/PerftTest.cs
@@ -0,0 +1,58 @@
+﻿using CommandLine;
+using Rudim.Board;
+using Rudim.Common;
+using System;
+using System.Diagnostics;
+using System.Diagnostics.CodeAnalysis;
+
+namespace Rudim.Test.Perft
+{
+
+    public static class PerftTest
+    {
+        public static void  Perft(int depth, ulong nodes, string position)
+        {
+            var timer = new Stopwatch();
+
+            History.ClearBoardHistory();
+
+            timer.Start();
+
+            var boardState = BoardState.ParseFEN(position);
+            PerftDriver.ResetNodeCount();
+            PerftDriver.Traverse(boardState, depth);
+
+            timer.Stop();
+
+            if(nodes != PerftDriver.Nodes){
+                Console.WriteLine($"There's a difference Expected: {nodes} Actual: {PerftDriver.Nodes}");
+                Environment.Exit(2);
+            }
+
+            History.ClearBoardHistory();
+            Console.WriteLine($"Execution Time: {timer.ElapsedMilliseconds} ms for {boardState} at depth {depth}");
+        }
+
+        public static void PerftDebug()
+        {
+            var depth = 2;
+
+            var boardState = BoardState.ParseFEN(Helpers.KiwiPeteFEN);
+            ulong total = 0;
+            boardState.GenerateMoves();
+            foreach (var move in boardState.Moves)
+            {
+                PerftDriver.ResetNodeCount();
+                boardState.MakeMove(move);
+
+                if (!boardState.IsInCheck(boardState.SideToMove.Other()))
+                    PerftDriver.Traverse(boardState, depth - 1);
+
+                total += PerftDriver.Nodes;
+                Console.WriteLine(move.Source.ToString() + move.Target.ToString() + " " + PerftDriver.Nodes + " " + move.Type.ToString());
+                boardState.UnmakeMove(move);
+            }
+            Console.WriteLine(total.ToString());
+        }
+    }
+}
--- Rudim/Program.cs
+++ Rudim/Program.cs
@@ -1,10 +1,16 @@
 ﻿using BenchmarkDotNet.Attributes;
 using BenchmarkDotNet.Running;
+using BenchmarkDotNet.Toolchains.DotNetCli;
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
 using Rudim.Board;
 using Rudim.CLI;
 using Rudim.Common;
 using Rudim.Search;
+using Rudim.Test.Perft;
+using System;
 using System.Collections.Generic;
+using System.Dynamic;
 using System.Threading;
 
 namespace Rudim
@@ -17,10 +23,56 @@ namespace Rudim
             {
                 BenchmarkRunner.Run<Benchmark>();
             }
+            else if (args.Length >= 1 && args[0] == "--perft")
+            {
+
+                var data = new List<PerftData> {
+                     new(0, 1, Helpers.StartingFEN),
+                     new(1, 20, Helpers.StartingFEN),
+                     new(2, 400, Helpers.StartingFEN),
+                     new(3, 8_902, Helpers.StartingFEN),
+                     new(4, 197_281, Helpers.StartingFEN),
+                     new(5, 4_865_609, Helpers.StartingFEN),
+                     new(6, 119_060_324, Helpers.StartingFEN),
+                     new(1, 48, Helpers.KiwiPeteFEN),
+                     new(2, 2_039, Helpers.KiwiPeteFEN),
+                     new(3, 97_862, Helpers.KiwiPeteFEN),
+                     new(4, 4_085_603, Helpers.KiwiPeteFEN),
+                     new(5, 193_690_690, Helpers.KiwiPeteFEN),
+                     new(1, 14, Helpers.EndgameFEN),
+                     new(2, 191, Helpers.EndgameFEN),
+                     new(3, 2_812, Helpers.EndgameFEN),
+                     new(4, 43_238, Helpers.EndgameFEN),
+                     new(5, 674_624, Helpers.EndgameFEN),
+                     new(6, 11_030_083, Helpers.EndgameFEN),
+                     new(7, 178_633_661, Helpers.EndgameFEN),
+                };
+                foreach (var item in data)
+                {
+                    PerftTest.Perft(item.Depth, item.Nodes, item.Position);
+                }
+            }
             else
             {
                 CliClient.Run();
             }
+        }
+    }
+
+    internal class PerftData
+    {
+        public int Depth { get; set; }
+
+        public ulong Nodes { get; set; }
+
+        public string Position { get; set; }
+
+        public PerftData(int depth, ulong nodes, string position)
+        {
+            Depth = depth;
+            Nodes = nodes;
+            Position = position;
+
         }
     }
 
