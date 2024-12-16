b9a3a3cfc7e843ecab090b21d33c9de03589c5b5
znxftw <vishnu2101@gmail.com>
2024-12-16 10:28:30 +0530

    exclude magics exception from coverage

--- Rudim/Common/ExceededMaximumRetryException.cs
+++ Rudim/Common/ExceededMaximumRetryException.cs
@@ -1,7 +1,9 @@
 ﻿using System;
+using System.Diagnostics.CodeAnalysis;
 using System.Runtime.Serialization;
 
 namespace Rudim.Common
 {
+    [ExcludeFromCodeCoverage]
     internal class ExceededMaximumRetryException(string message) : Exception(message);
 }
