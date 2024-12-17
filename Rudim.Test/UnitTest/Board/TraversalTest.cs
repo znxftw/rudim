aee47789e2b51df879128a1c2e9557c295cfbcd5
znxftw <vishnu2101@gmail.com>
2024-12-17 21:08:50 +0530

    fix test values

--- Rudim.Test/UnitTest/Board/TraversalTest.cs
+++ Rudim.Test/UnitTest/Board/TraversalTest.cs
@@ -13,10 +13,10 @@ namespace Rudim.Test.UnitTest.Board
         // This helps keep track if certain optimizations are good enough to make up for the extra time spent
         // Compare time spent with and without the change before updating the keys
         [Theory]
-        [InlineData(Helpers.StartingFEN, 3804303, 5, 8)]
-        [InlineData(Helpers.EndgameFEN, 1130154, 45, 9)]
-        [InlineData(Helpers.AdvancedMoveFEN, 6689081, 1520, 8)]
-        [InlineData(Helpers.KiwiPeteFEN, 10605420, -18, 8)]
+        [InlineData(Helpers.StartingFEN, 3071034, 5, 8)]
+        [InlineData(Helpers.EndgameFEN, 1164799, 45, 9)]
+        [InlineData(Helpers.AdvancedMoveFEN, 5351062, 1520, 8)]
+        [InlineData(Helpers.KiwiPeteFEN, 10923944, -60, 8)]
         public void ShouldTraverseDeterministically(string position, int expectedNodes, int expectedScore, int depth)
         {
             Global.Reset();
