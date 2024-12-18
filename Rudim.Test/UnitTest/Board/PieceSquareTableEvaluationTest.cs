c61d54bf69b0833daeb154aaca2d10e6d3dd251f
Vishnu Bhagyanath <vishnu2101@gmail.com>
2024-12-18 18:19:11 +0530

    eval: optimize & use Rofchade values (#47)
    - Taper all pieces, not just king
    - Avoids extra PhaseFactor multiplications every time, does it at the end
    - Use Rofchade values, previously using Simplified Values which aren't very strong. ref: https://www.chessprogramming.org/PeSTO%27s_Evaluation_Function
    - Avoid extra looping to calculate material value - we are already adding each piece's postion, so just precompute the position tables `+= MaterialValue`
    - Seeing some huge improvements with this

---
+++ Rudim.Test/UnitTest/Board/PieceSquareTableEvaluationTest.cs
@@ -0,0 +1,26 @@
+using Rudim.Board;
+using Rudim.Test.Util;
+using Xunit;
+using Helpers = Rudim.Common.Helpers;
+
+namespace Rudim.Test.UnitTest.Board
+{
+    public class PieceSquareTableEvaluationTest
+    {
+        [Theory]
+        [InlineData("rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1", 0)] // White
+        [InlineData("rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR b KQkq - 0 1", 0)] // Black
+        [InlineData(Helpers.EndgameFEN, 37)]
+        [InlineData(Helpers.KiwiPeteFEN, 56)]
+        [InlineData(Helpers.AdvancedMoveFEN, 495)]
+        
+        public void ShouldReturnConsistentScoreForGivenPosition(string fen, int expectedScore)
+        {
+            BoardState boardState = BoardState.ParseFEN(fen);
+
+            int actualScore = PieceSquareTableEvaluation.Evaluate(boardState);
+
+            Assert.Equal(expectedScore, actualScore);
+        }
+    }
+}
