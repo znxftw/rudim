55351b7fc32c1cd7741295483b44fda6c4667c8b
znxftw <vishnu2101@gmail.com>
2026-04-29 19:56:40 +0530

    square tests

--- src/common/square.rs
+++ src/common/square.rs
@@ -1,5 +1,7 @@
 use std::fmt;
 
+// TODO: Relook at optimizations here if reqd.
+#[rustfmt::skip]
 #[derive(Debug, Clone, Copy, PartialEq, Eq, Hash, PartialOrd, Ord)]
 pub enum Square {
     A8 = 0, B8, C8, D8, E8, F8, G8, H8,
@@ -17,7 +19,18 @@ impl Square {
     pub const ALL_SQUARES: usize = 64;
 }
 
-const SQUARES: [Square; 65] = [Square::A8, Square::B8, Square::C8, Square::D8, Square::E8, Square::F8, Square::G8, Square::H8, Square::A7, Square::B7, Square::C7, Square::D7, Square::E7, Square::F7, Square::G7, Square::H7, Square::A6, Square::B6, Square::C6, Square::D6, Square::E6, Square::F6, Square::G6, Square::H6, Square::A5, Square::B5, Square::C5, Square::D5, Square::E5, Square::F5, Square::G5, Square::H5, Square::A4, Square::B4, Square::C4, Square::D4, Square::E4, Square::F4, Square::G4, Square::H4, Square::A3, Square::B3, Square::C3, Square::D3, Square::E3, Square::F3, Square::G3, Square::H3, Square::A2, Square::B2, Square::C2, Square::D2, Square::E2, Square::F2, Square::G2, Square::H2, Square::A1, Square::B1, Square::C1, Square::D1, Square::E1, Square::F1, Square::G1, Square::H1, Square::NoSquare];
+#[rustfmt::skip]
+const SQUARES: [Square; 65] = [
+    Square::A8, Square::B8, Square::C8, Square::D8, Square::E8, Square::F8, Square::G8, Square::H8,
+    Square::A7, Square::B7, Square::C7, Square::D7, Square::E7, Square::F7, Square::G7, Square::H7,
+    Square::A6, Square::B6, Square::C6, Square::D6, Square::E6, Square::F6, Square::G6, Square::H6,
+    Square::A5, Square::B5, Square::C5, Square::D5, Square::E5, Square::F5, Square::G5, Square::H5,
+    Square::A4, Square::B4, Square::C4, Square::D4, Square::E4, Square::F4, Square::G4, Square::H4,
+    Square::A3, Square::B3, Square::C3, Square::D3, Square::E3, Square::F3, Square::G3, Square::H3,
+    Square::A2, Square::B2, Square::C2, Square::D2, Square::E2, Square::F2, Square::G2, Square::H2,
+    Square::A1, Square::B1, Square::C1, Square::D1, Square::E1, Square::F1, Square::G1, Square::H1,
+    Square::NoSquare,
+];
 
 impl From<usize> for Square {
     fn from(value: usize) -> Self {
@@ -40,14 +53,49 @@ impl fmt::Display for Square {
         if *self == Square::NoSquare {
             return write!(f, "-");
         }
-        
+
         let sq = *self as usize;
         let file = (sq % 8) as u8;
         let rank = 7 - (sq / 8) as u8;
-        
+
         let file_char = (b'a' + file) as char;
         let rank_char = (b'1' + rank) as char;
-        
+
         write!(f, "{}{}", file_char, rank_char)
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_square_from_usize() {
+        assert_eq!(Square::from(0), Square::A8);
+        assert_eq!(Square::from(28), Square::E5);
+        assert_eq!(Square::from(63), Square::H1);
+        assert_eq!(Square::from(64), Square::NoSquare);
+    }
+
+    #[test]
+    #[should_panic(expected = "Invalid square index: 65")]
+    fn test_square_from_usize_panics_on_invalid() {
+        let _ = Square::from(65);
+    }
+
+    #[test]
+    fn test_square_into_usize() {
+        assert_eq!(usize::from(Square::A8), 0);
+        assert_eq!(usize::from(Square::E5), 28);
+        assert_eq!(usize::from(Square::H1), 63);
+        assert_eq!(usize::from(Square::NoSquare), 64);
+    }
+
+    #[test]
+    fn test_square_display() {
+        assert_eq!(Square::A8.to_string(), "a8");
+        assert_eq!(Square::H1.to_string(), "h1");
+        assert_eq!(Square::E4.to_string(), "e4");
+        assert_eq!(Square::NoSquare.to_string(), "-");
     }
 }
--- src/lib.rs
+++ src/lib.rs
@@ -1,8 +1,8 @@
-pub mod common;
 pub mod bitboard;
 pub mod board;
+pub mod common;
+pub mod engine;
 pub mod eval;
+pub mod perft;
 pub mod search;
 pub mod uci;
-pub mod perft;
-pub mod engine;
