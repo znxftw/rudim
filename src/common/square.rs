774fc1e5ca293a1fb904860560422eb315d7d88c
znxftw <vishnu2101@gmail.com>
2026-05-27 19:39:50 +0530

    optz: idiomatic rust for SQUARES

--- src/common/square.rs
+++ src/common/square.rs
@@ -20,25 +20,75 @@ impl Square {
     pub const ALL_SQUARES: usize = 64;
 }
 
-#[rustfmt::skip]
-const SQUARES: [Square; 65] = [
-    Square::A8, Square::B8, Square::C8, Square::D8, Square::E8, Square::F8, Square::G8, Square::H8,
-    Square::A7, Square::B7, Square::C7, Square::D7, Square::E7, Square::F7, Square::G7, Square::H7,
-    Square::A6, Square::B6, Square::C6, Square::D6, Square::E6, Square::F6, Square::G6, Square::H6,
-    Square::A5, Square::B5, Square::C5, Square::D5, Square::E5, Square::F5, Square::G5, Square::H5,
-    Square::A4, Square::B4, Square::C4, Square::D4, Square::E4, Square::F4, Square::G4, Square::H4,
-    Square::A3, Square::B3, Square::C3, Square::D3, Square::E3, Square::F3, Square::G3, Square::H3,
-    Square::A2, Square::B2, Square::C2, Square::D2, Square::E2, Square::F2, Square::G2, Square::H2,
-    Square::A1, Square::B1, Square::C1, Square::D1, Square::E1, Square::F1, Square::G1, Square::H1,
-    Square::NoSquare,
-];
-
 impl From<usize> for Square {
     fn from(value: usize) -> Self {
-        if value <= 64 {
-            SQUARES[value]
-        } else {
-            panic!("Invalid square index: {}", value)
+        match value {
+            0 => Square::A8,
+            1 => Square::B8,
+            2 => Square::C8,
+            3 => Square::D8,
+            4 => Square::E8,
+            5 => Square::F8,
+            6 => Square::G8,
+            7 => Square::H8,
+            8 => Square::A7,
+            9 => Square::B7,
+            10 => Square::C7,
+            11 => Square::D7,
+            12 => Square::E7,
+            13 => Square::F7,
+            14 => Square::G7,
+            15 => Square::H7,
+            16 => Square::A6,
+            17 => Square::B6,
+            18 => Square::C6,
+            19 => Square::D6,
+            20 => Square::E6,
+            21 => Square::F6,
+            22 => Square::G6,
+            23 => Square::H6,
+            24 => Square::A5,
+            25 => Square::B5,
+            26 => Square::C5,
+            27 => Square::D5,
+            28 => Square::E5,
+            29 => Square::F5,
+            30 => Square::G5,
+            31 => Square::H5,
+            32 => Square::A4,
+            33 => Square::B4,
+            34 => Square::C4,
+            35 => Square::D4,
+            36 => Square::E4,
+            37 => Square::F4,
+            38 => Square::G4,
+            39 => Square::H4,
+            40 => Square::A3,
+            41 => Square::B3,
+            42 => Square::C3,
+            43 => Square::D3,
+            44 => Square::E3,
+            45 => Square::F3,
+            46 => Square::G3,
+            47 => Square::H3,
+            48 => Square::A2,
+            49 => Square::B2,
+            50 => Square::C2,
+            51 => Square::D2,
+            52 => Square::E2,
+            53 => Square::F2,
+            54 => Square::G2,
+            55 => Square::H2,
+            56 => Square::A1,
+            57 => Square::B1,
+            58 => Square::C1,
+            59 => Square::D1,
+            60 => Square::E1,
+            61 => Square::F1,
+            62 => Square::G1,
+            63 => Square::H1,
+            64 => Square::NoSquare,
+            _ => panic!("Invalid square index: {}", value),
         }
     }
 }
