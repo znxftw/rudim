5f6cbe09c23c99e4760c9d47e21d3b08f1ccf6c4
znxftw <vishnu2101@gmail.com>
2026-08-06 00:14:12 +0530

    refactor: remove bitflags dependency
    newtype u8 instead

--- src/common/castle.rs
+++ src/common/castle.rs
@@ -1,13 +1,65 @@
-use bitflags::bitflags;
+use std::ops::{BitAnd, BitAndAssign, BitOr, BitOrAssign};
 
-bitflags! {
-    #[derive(Debug, Clone, Copy, PartialEq, Eq, Hash)]
-    pub struct Castle: u8 {
-        const NONE = 0;
-        const WHITE_SHORT = 1;
-        const WHITE_LONG = 2;
-        const BLACK_SHORT = 4;
-        const BLACK_LONG = 8;
+#[derive(Debug, Clone, Copy, PartialEq, Eq, Hash, Default)]
+pub struct Castle(pub u8);
+
+impl Castle {
+    pub const NONE: Self = Self(0);
+    pub const WHITE_SHORT: Self = Self(1);
+    pub const WHITE_LONG: Self = Self(2);
+    pub const BLACK_SHORT: Self = Self(4);
+    pub const BLACK_LONG: Self = Self(8);
+
+    #[inline]
+    pub const fn bits(&self) -> u8 {
+        self.0
+    }
+
+    #[inline]
+    pub const fn from_bits_retain(bits: u8) -> Self {
+        Self(bits)
+    }
+
+    #[inline]
+    pub const fn contains(&self, other: Self) -> bool {
+        (self.0 & other.0) == other.0
+    }
+
+    #[inline]
+    pub fn remove(&mut self, other: Self) {
+        self.0 &= !other.0;
+    }
+}
+
+impl BitOr for Castle {
+    type Output = Self;
+
+    #[inline]
+    fn bitor(self, rhs: Self) -> Self {
+        Self(self.0 | rhs.0)
+    }
+}
+
+impl BitOrAssign for Castle {
+    #[inline]
+    fn bitor_assign(&mut self, rhs: Self) {
+        self.0 |= rhs.0;
+    }
+}
+
+impl BitAnd for Castle {
+    type Output = Self;
+
+    #[inline]
+    fn bitand(self, rhs: Self) -> Self {
+        Self(self.0 & rhs.0)
+    }
+}
+
+impl BitAndAssign for Castle {
+    #[inline]
+    fn bitand_assign(&mut self, rhs: Self) {
+        self.0 &= rhs.0;
     }
 }
 
